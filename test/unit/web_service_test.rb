c6e4104419bedfee79c435928ff6585e9d18c3fb
Hallx <faheem+git@gmail.com>
2013-05-23 18:09:27 +0200

    added test for web service class

---
+++ test/unit/web_service_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class WebServiceTest < ActiveSupport::TestCase
+  test "verify there is no nil company in array" do
+    
+    companies = WebService.get_companies("kentor it kentor ab")
+    
+    assert_equal(10, companies.size)
+    for company in companies do
+      assert_not_nil(company)
+      assert(!company.empty?)
+      
+      assert_not_nil(company[:name])
+    end
+  end
+  
+  test "verify there are ten items array" do
+    companies = WebService.get_companies("kento it kentor ab")
+    assert_equal(10, companies.size)
+  end
+end
